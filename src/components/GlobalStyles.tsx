6f6555725abe50994c82ac09c842c24dd92688a0
Joe Alden <me@joealden.com>
2018-10-22 01:13:47 +0100

    Add box-sizing: border-box to global css rules

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,9 +1,15 @@
 import { createGlobalStyle } from "../utils/styled-components";
 
 const GlobalStyles = createGlobalStyle`
+  * {
+    box-sizing: border-box;
+  }
+  
   body {
     margin: 0;
+    overflow: hidden;
     background-color: ${props => props.theme.primaryColor};
+    color: ${props => props.theme.secondaryColor};
     transition: background-color 0.3s ease;
   }
 `;
