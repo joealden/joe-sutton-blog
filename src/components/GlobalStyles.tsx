c318494cf88d5fd37f6893e8a514d5cfd388590d
Joe Alden <me@joealden.com>
2018-10-22 15:11:22 +0100

    Add fonts

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -1,11 +1,19 @@
 import { createGlobalStyle } from "../utils/styled-components";
+import { withPrefix } from "gatsby";
 
 const GlobalStyles = createGlobalStyle`
+  @font-face {
+    font-family: SuisseIntl;
+    src: url(${withPrefix("SuisseIntl-Regular.woff")});
+  }
+
   * {
     box-sizing: border-box;
+    font-family: SuisseIntl;
   }
   
   body {
+    font-family: SuisseIntl;
     margin: 0;
     overflow: hidden;
     background-color: ${props => props.theme.primaryColor};
