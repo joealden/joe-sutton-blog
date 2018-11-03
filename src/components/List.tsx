d8dae7435fe186dde666752fdf131c12b5b7b2a5
Joe Alden <me@joealden.com>
2018-11-03 17:08:21 +0000

    Implement hovering behaviour

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -7,86 +7,44 @@ interface ListProps {
   toggleInfo: () => void;
 }
 
-const List: React.SFC<ListProps> = ({ toggleInfo }) => {
-  return (
-    <ListWrapper>
-      <PaddingListItem />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Akademi"
-        catagory="Website"
-        link="https://studioakademi.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Rally Interactive"
-        catagory="Brand Identity"
-        link="http://rallyinteractive.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Leo et Violette"
-        catagory="Website"
-        link="https://www.leoetviolette.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Bear Grylls"
-        catagory="Website"
-        link="http://www.beargrylls.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Akademi"
-        catagory="Website"
-        link="https://studioakademi.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Rally Interactive"
-        catagory="Brand Identity"
-        link="http://rallyinteractive.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Leo et Violette"
-        catagory="Website"
-        link="https://www.leoetviolette.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Bear Grylls"
-        catagory="Website"
-        link="http://www.beargrylls.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Akademi"
-        catagory="Website"
-        link="https://studioakademi.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Rally Interactive"
-        catagory="Brand Identity"
-        link="http://rallyinteractive.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Leo et Violette"
-        catagory="Website"
-        link="https://www.leoetviolette.com/"
-      />
-      <ListItem
-        toggleInfo={toggleInfo}
-        title="Bear Grylls"
-        catagory="Website"
-        link="http://www.beargrylls.com/"
-      />
-      <PaddingListItem />
-    </ListWrapper>
-  );
-};
+interface ListState {
+  activeItemId: string;
+}
+
+class List extends React.Component<ListProps, ListState> {
+  state = {
+    /* Dependent on real data and current filtering */
+    activeItemId: "1"
+  };
+
+  setCurrentlyActiveItem = (id: string) => this.setState({ activeItemId: id });
+
+  render() {
+    const { toggleInfo } = this.props;
+    const { activeItemId } = this.state;
+
+    return (
+      <ListWrapper>
+        <PaddingListItem />
+        {tempData.map(item => {
+          const { id } = item;
+          const className = activeItemId === id ? "active-item" : "";
+
+          return (
+            <ListItem
+              toggleInfo={toggleInfo}
+              setCurrentlyActiveItem={this.setCurrentlyActiveItem}
+              className={className}
+              key={id}
+              {...item}
+            />
+          );
+        })}
+        <PaddingListItem />
+      </ListWrapper>
+    );
+  }
+}
 
 export default List;
 
@@ -94,22 +52,49 @@ const ListWrapper = styled.ul`
   list-style: none;
   padding: 0;
   margin: 0;
+
+  li.active-item {
+    & > a > div:nth-child(2) > div:nth-child(2),
+    & > a > div:nth-child(1),
+    & > a > div:nth-child(2) > div:first-child > span:last-child svg polygon,
+    & > a > div:nth-child(2) > div:last-child > span {
+      opacity: 1;
+      visibility: visible;
+    }
+
+    & > a > div:nth-child(2) > div:first-child span:first-child {
+      color: ${props => props.theme.foregroundColor};
+    }
+
+    & > a > div:nth-child(2) > div:first-child > span:last-child svg polygon {
+      fill: ${props => props.theme.foregroundColor};
+    }
+  }
 `;
 
 interface ListItemProps {
+  toggleInfo: () => void;
+  setCurrentlyActiveItem: (id: string) => void;
+  className: string;
   title: string;
   catagory: string;
   link: string;
-  toggleInfo: () => void;
+  id: string;
 }
 
 const ListItem: React.SFC<ListItemProps> = ({
+  toggleInfo,
+  setCurrentlyActiveItem,
+  className,
   title,
   catagory,
   link,
-  toggleInfo
+  id
 }) => (
-  <StyledLi>
+  <StyledLi
+    className={className}
+    onMouseOver={() => setCurrentlyActiveItem(id)}
+  >
     <a href={link} rel="noreferrer noopener" target="_blank">
       <InfoContainer>
         <span
@@ -144,26 +129,8 @@ const StyledLi = styled.li`
     cursor: pointer;
     text-decoration: none;
     color: ${props => props.theme.foregroundColor};
-    /* Think about ally of this */
+    /* Think about a11y of this */
     outline: none;
-  }
-
-  &:hover {
-    & > a > div:nth-child(2) > div:nth-child(2),
-    & > a > div:nth-child(1) > span,
-    & > a > div:nth-child(2) > div:first-child > span:last-child svg polygon,
-    & > a > div:nth-child(2) > div:last-child > span {
-      opacity: 1;
-      visibility: visible;
-    }
-
-    & > a > div:nth-child(2) > div:first-child span:first-child {
-      color: ${props => props.theme.foregroundColor};
-    }
-
-    & > a > div:nth-child(2) > div:first-child > span:last-child svg polygon {
-      fill: ${props => props.theme.foregroundColor};
-    }
   }
 `;
 
@@ -174,16 +141,20 @@ const InfoContainer = styled.div`
   justify-content: center;
   overflow: hidden;
 
+  visibility: hidden;
+  opacity: 0;
+  margin-bottom: 0.6vw;
+
+  transition: opacity ${props => props.theme.transition},
+    visibility ${props => props.theme.transition};
+
   span {
-    transition: opacity ${props => props.theme.transition},
-      visibility ${props => props.theme.transition},
-      color ${props => props.theme.transition};
+    line-height: 70%;
+    transition: color ${props => props.theme.transition};
+    writing-mode: vertical-lr;
+    transform: rotate(180deg);
 
-    visibility: hidden;
-    transform: rotate(-90deg);
-
-    margin-bottom: 1.4vw;
-    opacity: 0;
+    padding: 0.6vw;
     user-select: none;
 
     &:hover {
@@ -248,7 +219,8 @@ const CatagoryContainer = styled.div`
     opacity: 0;
     visibility: hidden;
     transition: opacity ${props => props.theme.transition},
-      visibility ${props => props.theme.transition};
+      visibility ${props => props.theme.transition},
+      color ${props => props.theme.transition};
   }
 `;
 
@@ -269,9 +241,132 @@ const PaddingListItem: React.SFC = () => (
 
 const EmptyStyledLi = styled(StyledLi)`
   height: 8vw;
-  cursor: default;
 
   a {
+    cursor: default;
     height: 8vw;
   }
 `;
+
+const tempData = [
+  {
+    id: "1",
+    title: "Akademi",
+    catagory: "Website",
+    link: "https://studioakademi.com/"
+  },
+  {
+    id: "2",
+    title: "Rally Interactive",
+    catagory: "Brand Identity",
+    link: "http://rallyinteractive.com/"
+  },
+  {
+    id: "3",
+    title: "Leo et Violette",
+    catagory: "Website",
+    link: "https://www.leoetviolette.com/"
+  },
+  {
+    id: "4",
+    title: "Bear Grylls",
+    catagory: "Website",
+    link: "http://www.beargrylls.com/"
+  },
+  {
+    id: "5",
+    title: "Akademi",
+    catagory: "Website",
+    link: "https://studioakademi.com/"
+  },
+  {
+    id: "6",
+    title: "Rally Interactive",
+    catagory: "Brand Identity",
+    link: "http://rallyinteractive.com/"
+  },
+  {
+    id: "7",
+    title: "Leo et Violette",
+    catagory: "Website",
+    link: "https://www.leoetviolette.com/"
+  },
+  {
+    id: "8",
+    title: "Bear Grylls",
+    catagory: "Website",
+    link: "http://www.beargrylls.com/"
+  },
+  {
+    id: "9",
+    title: "Akademi",
+    catagory: "Website",
+    link: "https://studioakademi.com/"
+  },
+  {
+    id: "10",
+    title: "Rally Interactive",
+    catagory: "Brand Identity",
+    link: "http://rallyinteractive.com/"
+  },
+  {
+    id: "11",
+    title: "Leo et Violette",
+    catagory: "Website",
+    link: "https://www.leoetviolette.com/"
+  },
+  {
+    id: "12",
+    title: "Bear Grylls",
+    catagory: "Website",
+    link: "http://www.beargrylls.com/"
+  },
+  {
+    id: "13",
+    title: "Akademi",
+    catagory: "Website",
+    link: "https://studioakademi.com/"
+  },
+  {
+    id: "14",
+    title: "Rally Interactive",
+    catagory: "Brand Identity",
+    link: "http://rallyinteractive.com/"
+  },
+  {
+    id: "15",
+    title: "Leo et Violette",
+    catagory: "Website",
+    link: "https://www.leoetviolette.com/"
+  },
+  {
+    id: "16",
+    title: "Bear Grylls",
+    catagory: "Website",
+    link: "http://www.beargrylls.com/"
+  },
+  {
+    id: "17",
+    title: "Akademi",
+    catagory: "Website",
+    link: "https://studioakademi.com/"
+  },
+  {
+    id: "18",
+    title: "Rally Interactive",
+    catagory: "Brand Identity",
+    link: "http://rallyinteractive.com/"
+  },
+  {
+    id: "19",
+    title: "Leo et Violette",
+    catagory: "Website",
+    link: "https://www.leoetviolette.com/"
+  },
+  {
+    id: "20",
+    title: "Bear Grylls",
+    catagory: "Website",
+    link: "http://www.beargrylls.com/"
+  }
+];
