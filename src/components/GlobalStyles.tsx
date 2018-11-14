e1e616c124838b70b564c039ebd110a8c62d2a30
Joe Alden <me@joealden.com>
2018-11-14 11:57:19 +0000

    Move font size media queries into static styles.css file so they actually take effect.

--- src/components/GlobalStyles.tsx
+++ src/components/GlobalStyles.tsx
@@ -3,16 +3,6 @@ import { createGlobalStyle } from "../utils/styled-components";
 /* All non-dynamic styles have been moved from here to styles.css */
 
 const GlobalStyles = createGlobalStyle`
-  html {    
-    /* 2000 number is a placeholder */
-    @media screen and (max-width: 2000px) {
-      font-size: 18px;
-    }
-    @media screen and (max-width: 1300px) {
-      font-size: 16px;
-    }
-  }
-
   body {
     background-color: ${props => props.theme.backgroundColor};
     color: ${props => props.theme.foregroundColor};
