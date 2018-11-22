3e21628c7bf500a716c0da1f7add8a82456a2f91
Joe Alden <me@joealden.com>
2018-11-22 20:01:24 +0000

    Remove active sort indicator transition

--- src/components/ActiveSortIndicator.tsx
+++ src/components/ActiveSortIndicator.tsx
@@ -21,15 +21,15 @@ const ActiveSortIndicator: React.FunctionComponent<ASIProps> = ({ active }) => (
 export default ActiveSortIndicator;
 
 const ASIWrapper = styled.div`
-  div {
+  /* div {
     transition: opacity ${props => props.theme.transition},
       visibility ${props => props.theme.transition};
-  }
+  } */
 `;
 
 const Circle: React.FunctionComponent = () => (
   <StyledSVG height="12" width="12">
-    <circle cx="6" cy="6" r="5" />
+    <circle cx="6" cy="6" r="4" />
   </StyledSVG>
 );
 
