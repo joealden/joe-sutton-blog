0ff00355a8df7708d1cac9cba291e7386934e8da
Joe Alden <me@joealden.com>
2018-11-23 14:16:16 +0000

    Remove Suspense component

--- src/components/Listed.tsx
+++ src/components/Listed.tsx
@@ -37,14 +37,12 @@ class Listed extends React.Component<ListedProps, ListedState> {
     return (
       <ThemeProvider theme={currentTheme}>
         <>
-          <React.Suspense fallback={<div>Loading...</div>}>
-            <MediaQuery maxWidth={1023}>
-              <MobileSite toggleTheme={this.toggleTheme} posts={posts} />
-            </MediaQuery>
-            <MediaQuery minWidth={1024}>
-              <DesktopSite toggleTheme={this.toggleTheme} posts={posts} />) }
-            </MediaQuery>
-          </React.Suspense>
+          <MediaQuery maxWidth={1023}>
+            <MobileSite toggleTheme={this.toggleTheme} posts={posts} />
+          </MediaQuery>
+          <MediaQuery minWidth={1024}>
+            <DesktopSite toggleTheme={this.toggleTheme} posts={posts} />) }
+          </MediaQuery>
           <GlobalStyles />
         </>
       </ThemeProvider>
