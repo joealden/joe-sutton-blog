ea8a776330596203ad641dadd9e38dd883e5b0cb
Joe Alden <me@joealden.com>
2018-11-30 15:35:54 +0000

    Add filter line animation

--- src/components/desktop/Header.tsx
+++ src/components/desktop/Header.tsx
@@ -1,11 +1,37 @@
 import React from "react";
-import styled, { css } from "../../utils/styled-components";
+import styled, { css, keyframes } from "../../utils/styled-components";
 
 import { FilterSortBy } from "../../utils/types";
 
 import Circle from "../icons/Circle";
 import Logo from "../icons/Logo";
 import BackToTop from "../icons/BackToTop";
+
+enum FilterLineTransition {
+  Initial,
+  Enter,
+  Leave,
+  /* Two different state so that reload is actually triggered */
+  Reload1,
+  Reload2
+}
+
+const calculateLineTransitionClass = (
+  filterLineTransition: FilterLineTransition
+) => {
+  switch (filterLineTransition) {
+    case FilterLineTransition.Initial:
+      return "";
+    case FilterLineTransition.Enter:
+      return "filter-line-enter";
+    case FilterLineTransition.Leave:
+      return "filter-line-leave";
+    case FilterLineTransition.Reload1:
+      return "filter-line-reload-1";
+    case FilterLineTransition.Reload2:
+      return "filter-line-reload-2";
+  }
+};
 
 interface HeaderProps {
   sortBy: FilterSortBy;
@@ -21,11 +47,13 @@ interface HeaderProps {
 
 interface HeaderState {
   showBackToTopButton: boolean;
+  filterLineTransition: FilterLineTransition;
 }
 
 class Header extends React.Component<HeaderProps, HeaderState> {
   state = {
-    showBackToTopButton: false
+    showBackToTopButton: false,
+    filterLineTransition: FilterLineTransition.Initial
   };
 
   shouldBackToTopButtonBeShown = () => {
@@ -42,6 +70,50 @@ class Header extends React.Component<HeaderProps, HeaderState> {
     }
   };
 
+  /* TODO: Remove console.logs when line actually implemented */
+  setLineTransitionState = (prevProps: HeaderProps) => {
+    const newProps = this.props;
+    const { state } = this;
+
+    if (
+      (state.filterLineTransition === FilterLineTransition.Initial ||
+        state.filterLineTransition === FilterLineTransition.Leave) &&
+      (newProps.sortBy !== FilterSortBy.NewestFirst ||
+        newProps.selectedCategory !== null ||
+        newProps.selectedTags.length !== 0)
+    ) {
+      this.setState({ filterLineTransition: FilterLineTransition.Enter });
+      console.log("Line enter");
+    } else if (
+      state.filterLineTransition === FilterLineTransition.Enter ||
+      state.filterLineTransition === FilterLineTransition.Reload1 ||
+      state.filterLineTransition === FilterLineTransition.Reload2
+    ) {
+      if (
+        newProps.sortBy === FilterSortBy.NewestFirst &&
+        newProps.selectedCategory === null &&
+        newProps.selectedTags.length === 0
+      ) {
+        this.setState({ filterLineTransition: FilterLineTransition.Leave });
+        console.log("Line leave");
+      } else if (
+        prevProps.sortBy !== newProps.sortBy ||
+        prevProps.selectedCategory !== newProps.selectedCategory ||
+        prevProps.selectedTags !== newProps.selectedTags
+      ) {
+        if (state.filterLineTransition === FilterLineTransition.Reload1) {
+          this.setState({ filterLineTransition: FilterLineTransition.Reload2 });
+        } else {
+          this.setState({ filterLineTransition: FilterLineTransition.Reload1 });
+        }
+
+        console.log("Line reload");
+      }
+    } else {
+      console.log("not state change occured");
+    }
+  };
+
   /* https://developers.google.com/web/updates/2016/06/passive-event-listeners */
   componentDidMount() {
     window.addEventListener("scroll", this.shouldBackToTopButtonBeShown, {
@@ -51,6 +123,10 @@ class Header extends React.Component<HeaderProps, HeaderState> {
 
   componentWillUnmount() {
     window.removeEventListener("scroll", this.shouldBackToTopButtonBeShown);
+  }
+
+  componentDidUpdate(prevProps: HeaderProps) {
+    this.setLineTransitionState(prevProps);
   }
 
   logoClick = () => {
@@ -75,14 +151,8 @@ class Header extends React.Component<HeaderProps, HeaderState> {
       selectedTags
     } = this.props;
 
-    const { showBackToTopButton } = this.state;
+    const { showBackToTopButton, filterLineTransition } = this.state;
     const { logoClick } = this;
-
-    const sortApplied = sortBy !== FilterSortBy.NewestFirst;
-    const categoryApplied = selectedCategory !== null;
-    const tagsApplied = selectedTags.length !== 0;
-
-    const filterApplied = sortApplied || categoryApplied || tagsApplied;
 
     return (
       <HeaderWrapper>
@@ -92,7 +162,7 @@ class Header extends React.Component<HeaderProps, HeaderState> {
         <FilterAndBackToTopWrapper>
           <button
             onClick={openFilter}
-            className={filterApplied ? "filter-applied" : ""}
+            className={calculateLineTransitionClass(filterLineTransition)}
           >
             <span>Filter</span>
           </button>
@@ -153,20 +223,111 @@ const commonFlexHeaderStyles = css`
   overflow: hidden;
 `;
 
+/** --------------------------------------- /
+ * NOTE:
+ * This is horrible hacking code, please
+ * replace with JavaScript animations pronto
+ */
+
+const filterLineAnimation1 = keyframes`
+  from {
+    width: 0;
+  }
+
+  to {
+    width: 100%;
+  }
+`;
+
+const filterLineAnimation2 = keyframes`
+  from {
+    width: 100%;
+    margin-left: 0;
+  }
+
+  to {
+    width: 0;
+    margin-left: 100%;
+  }
+`;
+
+const filterLineAnimation3 = keyframes`
+  0% {
+    width: 100%;
+    margin-left: 0;
+  }
+
+  50% {
+    width: 0;
+    margin-left: 100%;
+  }
+
+  51% {
+    margin-left: 0;
+  }
+
+  100% {
+    width: 100%;
+    margin-left: 0;
+  }
+`;
+
+/* 'from' and 'to' used to trigger animation to reload */
+const filterLineAnimation4 = keyframes`
+  from {
+    width: 100%;
+    margin-left: 0;
+  }
+
+  50% {
+    width: 0;
+    margin-left: 100%;
+  }
+
+  51% {
+    margin-left: 0;
+  }
+
+  to {
+    width: 100%;
+    margin-left: 0;
+  }
+`;
+
+/* -------------------------------------- */
+
 const FilterAndBackToTopWrapper = styled.div`
   ${commonFlexHeaderStyles};
   border-left: 1px solid ${props => props.theme.lineColor};
   transition: border-color ${props => props.theme.transition};
 
   button:first-child {
-    span {
-      border-bottom: 1px solid ${props => props.theme.backgroundColor};
-      transition: border-color ${props => props.theme.transition};
-    }
-  }
+    /* NOTE: This offsets the bottom border height */
+    margin-top: 1px;
 
-  button:first-child.filter-applied span {
-    border-bottom: 1px solid ${props => props.theme.accentColor};
+    span:after {
+      content: "";
+      display: block;
+      width: 0;
+      height: 1px;
+      background-color: ${props => props.theme.accentColor};
+    }
+
+    &.filter-line-enter span:after {
+      animation: ${filterLineAnimation1} 0.3s ease forwards;
+    }
+
+    &.filter-line-leave span:after {
+      animation: ${filterLineAnimation2} 0.3s ease forwards;
+    }
+
+    &.filter-line-reload-1 span:after {
+      animation: ${filterLineAnimation3} 0.6s ease forwards;
+    }
+
+    &.filter-line-reload-2 span:after {
+      animation: ${filterLineAnimation4} 0.6s ease forwards;
+    }
   }
 
   button:last-child {
