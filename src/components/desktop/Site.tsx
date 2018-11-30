9b45596a23cacbe80628b4172b9f872d8b9dcaa9
Joe Alden <me@joealden.com>
2018-11-30 12:20:52 +0000

    Add blur on section open

--- src/components/desktop/Site.tsx
+++ src/components/desktop/Site.tsx
@@ -104,7 +104,8 @@ const Site: React.FunctionComponent<SiteProps> = ({
         <About isOpen={aboutOpen} close={closeAbout} />
         <HeaderAndListContainer
           style={{
-            opacity: anySectionIsOpen ? 0.5 : 1
+            opacity: anySectionIsOpen ? 0.5 : 1,
+            filter: anySectionIsOpen ? "blur(2.5px)" : ""
           }}
         >
           <Overlay
