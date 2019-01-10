eccbe176d6962ea6efddd7608d9affec1ac0d155
Joe Alden <me@joealden.com>
2019-01-10 22:12:49 +0000

    Update font.d.ts with a comment on why the file is needed

--- fonts.d.ts
+++ fonts.d.ts
@@ -1,2 +1,10 @@
-declare module "*.woff";
+/**
+ * NOTE:
+ *
+ * This declaration means that files that end in `.woff2`
+ * don't throw errors when imported like modules. This is
+ * needed to get the final location of the font in order
+ * to preload it in `src/html.tsx`.
+ */
+
 declare module "*.woff2";
